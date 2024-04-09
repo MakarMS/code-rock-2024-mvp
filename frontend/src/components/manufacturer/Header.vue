e865eda413fb6831156ad3016dbfb09a7dc28c77
Makar Mikhalchenko <makar.mikhalchenko@bk.ru>
2024-04-10 00:02:13 +0300

    feat: Add stub dashboard and header

--- frontend/src/components/manufacturer/Header.vue
+++ frontend/src/components/manufacturer/Header.vue
@@ -1,13 +1,22 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const {locale, availableLocales} = useI18n();
+
+const changeLanguage = () => {
+    locale.value = availableLocales[(availableLocales.indexOf(locale.value) + 1) % availableLocales.length];
+}
+</script>
 <template>
-    <header class="bg-orange-400 p-4 flex justify-between items-center">
+    <header class="bg-white text-orange-400 p-4 flex justify-between items-center shadow-lg">
         <div>
-            <router-link to="/manufacturer/routes" class="text-white font-semibold px-4 py-2 rounded-full hover:bg-opacity-0 hover:border-orange-400 border-2 border-transparent transition-all">
-                Маршруты
-            </router-link>
-            <router-link to="/manufacturer/products" class="text-white font-semibold px-4 py-2 rounded-full hover:bg-opacity-0 hover:border-orange-400 border-2 border-transparent transition-all ml-4">
-                Изделия
+            <router-link v-for="button in $tm('header.manufacturer_header')" :title="button.label" :to="'/manufacturer/' + button.route" class="font-semibold px-4 py-2 rounded-full hover:border-orange-400 border-2 border-transparent transition-all">
+                {{ button.label }}
             </router-link>
         </div>
+        <button :title="$t('titles.change_language')" class="relative" @click="changeLanguage">
+            <i class="pi pi-language text-orange-400 hover:text-orange-500 text-2xl"></i>
+        </button>
     </header>
 </template>
 
--- frontend/src/pages/manufacturer/DashboardPage.vue
+++ frontend/src/pages/manufacturer/DashboardPage.vue
@@ -5,8 +5,67 @@ import Header from "../../components/manufacturer/Header.vue";
 
 <template>
     <Header />
+    <div class="grid grid-cols-1 md:grid-cols-2 shadow-lg lg:grid-cols-4 gap-4 mt-10 ml-5 mr-5">
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-lg font-semibold mb-4">Total Sales</h2>
+            <p class="text-3xl font-bold text-orange-400">10,000 ₽</p>
+        </div>
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-lg font-semibold mb-4">Total Orders</h2>
+            <p class="text-3xl font-bold text-orange-400">100</p>
+        </div>
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-lg font-semibold mb-4">New Customers</h2>
+            <p class="text-3xl font-bold text-orange-400">50</p>
+        </div>
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-lg font-semibold mb-4">Pending Orders</h2>
+            <p class="text-3xl font-bold text-orange-400">10</p>
+        </div>
+    </div>
+    <div class="mt-10 ml-5 mr-5">
+        <h2 class="text-xl font-semibold mb-4">Most Popular Products</h2>
+        <ul>
+            <li class="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+                <div>
+                    <h3 class="text-lg font-semibold">Product 1</h3>
+                    <p class="text-sm">Category: Category 1</p>
+                </div>
+                <span class="text-lg font-bold text-orange-400">1000 ₽</span>
+            </li>
+            <li class="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+                <div>
+                    <h3 class="text-lg font-semibold">Product 2</h3>
+                    <p class="text-sm">Category: Category 2</p>
+                </div>
+                <span class="text-lg font-bold text-orange-400">900 ₽</span>
+            </li>
+            <li class="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+                <div>
+                    <h3 class="text-lg font-semibold">Product 3</h3>
+                    <p class="text-sm">Category: Category 3</p>
+                </div>
+                <span class="text-lg font-bold text-orange-400">800 ₽</span>
+            </li>
+            <li class="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+                <div>
+                    <h3 class="text-lg font-semibold">Product 4</h3>
+                    <p class="text-sm">Category: Category 4</p>
+                </div>
+                <span class="text-lg font-bold text-orange-400">700 ₽</span>
+            </li>
+            <li class="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+                <div>
+                    <h3 class="text-lg font-semibold">Product 5</h3>
+                    <p class="text-sm">Category: Category 5</p>
+                </div>
+                <span class="text-lg font-bold text-orange-400">600 ₽</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
 
 </style>
+
