42fb5e59a2fb156238f65d8c5bfc5105b8330839
Makar Mikhalchenko <makar.mikhalchenko@bk.ru>
2024-04-09 22:26:31 +0300

    refactor: Rename folder

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomePage from './pages/Landing/HomePage.vue'
+import HomePage from './pages/landing/HomePage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
